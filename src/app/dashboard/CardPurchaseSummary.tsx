4367a10368752a3e1d7e5bc3c5ec951619e39c2d
kumanboy <husandavronov13@gmail.com>
2025-01-18 19:38:31 +0500

    CardPurchaseSummary.tsx & CardExpensesSummary.tsx added

---
+++ src/app/dashboard/CardPurchaseSummary.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {useGetDashboardMetricsQuery} from "@/state/api";
+
+const CardPurchaseSummary = () => {
+    const {data, isLoading} = useGetDashboardMetricsQuery();
+
+    const purchaseData = data?.purchaseSummary || []
+
+
+    return (
+        <div className={` flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl`}>
+            {isLoading ? (<div className={`m-5`}>Loading... </div>) :
+                <>
+                    {/* HEADER */}
+                    <div>
+                        <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+                            Purchase Summary
+                        </h2>
+                        <hr />
+                    </div>
+                </>
+            }
+
+        </div>
+    )
+}
+export default CardPurchaseSummary
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,16 @@
 "use client"
 import CardPopularProducts from "@/app/dashboard/CardPopularProducts";
 import CardSalesSummary from "@/app/dashboard/CardSalesSummary";
+import CardPurchaseSummary from "@/app/dashboard/CardPurchaseSummary";
+import CardExpensesSummary from "@/app/dashboard/CardExpensesSummary";
 
 const Dashboard = () => {
     return (
         <div className={`grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows`}>
             <CardPopularProducts/>
             <CardSalesSummary/>
-            <div className={`row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500`}/>
-            <div className={`row-span-3 bg-gray-500`}/>
+            <CardPurchaseSummary/>
+            <CardExpensesSummary/>
             <div className={`row-span-1 xl:row-span-2`}/>
             <div className={`row-span-1 xl:row-span-2`}/>
             <div className={`row-span-1 xl:row-span-2`}/>
